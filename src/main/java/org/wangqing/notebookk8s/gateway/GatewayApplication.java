80a34158d2d18f7bb148906f157892744c681aad
Administrator <wzxmt@163.com>
2023-06-11 18:14:55 +0800

    1

--- src/main/java/org/wangqing/notebookk8s/gateway/GatewayApplication.java
+++ src/main/java/org/wangqing/notebookk8s/gateway/GatewayApplication.java
@@ -8,11 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
+@EnableApolloConfig
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+//在微服务应用启动中使用apollo配置中心获取配置信息
+public class SpringbootApolloApplication {
+　　public static void main(String[] args) {
+　　　　SpringApplication.run(SpringbootApolloApplication.class, args);
+　　}
+}
 
 }
